99800c92114648a5da17dc647f824553fb15f82a
akash <akash@adhiafunds.com>
2018-01-01 13:03:16 -0600

    Added Asset and Dynamic classes

---
+++ src/assetAllocater/Asset.java
@@ -0,0 +1,102 @@
+package assetAllocater;
+
+
+import stockGenie.BloombergAPICommunicator;
+
+public class Asset {
+	
+	private double price;
+	private int amount;
+	private String ticker;
+	private BloombergAPICommunicator.Sector sector; 
+	private double origValue;
+	
+	//Rule 1 fields 
+	private double lossLimitCutoff;
+		
+	//Rule 2 fields
+	private double growthLimitCutoff; 
+		
+	//Rule 3 fields
+	private double portPortionCutoff;
+	private double portPortionAfterReduction;
+	
+	
+	public Asset(double price, int amount, String ticker, BloombergAPICommunicator.Sector sector) {
+		super();
+		this.price = price;
+		this.amount = amount;
+		this.ticker = ticker;
+		this.origValue = amount * price;
+		this.sector = sector;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getTicker() {
+		return ticker;
+	}
+
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+	
+	public double getOrigValue() {
+		return origValue;
+	}
+	
+	public double valueNow() {
+		return amount * price;
+	}
+	
+	public BloombergAPICommunicator.Sector getSector() {
+		return sector;
+	}
+
+	public double getLossLimitCutoff() {
+		return lossLimitCutoff;
+	}
+
+	public void setLossLimitCutoff(double lossLimitCutoff) {
+		this.lossLimitCutoff = lossLimitCutoff;
+	}
+
+	public double getPortPortionCutoff() {
+		return portPortionCutoff;
+	}
+
+	public void setPortPortionCutoff(double portPortionCutoff) {
+		this.portPortionCutoff = portPortionCutoff;
+	}
+
+	public double getPortPortionAfterReduction() {
+		return portPortionAfterReduction;
+	}
+
+	public void setPortPortionAfterReduction(double portPortionAfterReduction) {
+		this.portPortionAfterReduction = portPortionAfterReduction;
+	}
+
+	public double getGrowthLimitCutoff() {
+		return growthLimitCutoff;
+	}
+
+	public void setGrowthLimitCutoff(double growthLimitCutoff) {
+		this.growthLimitCutoff = growthLimitCutoff;
+	}
+
+}
---
+++ src/assetAllocater/Dyanmic.java
@@ -0,0 +1,63 @@
+package assetAllocater;
+
+import java.util.ArrayList;
+import java.util.Date;
+import filters.Order;
+import stockGenie.BloombergAPICommunicator;
+
+
+public class Dyanmic {
+	
+	private double currentBalance;
+	private ArrayList<Asset> assets;
+	private ArrayList<Order> orderList;
+	private boolean stocksLeft;
+	
+	public void dailyRun(BloombergAPICommunicator bloomberg) {
+		//get asset names and amounts from trader's workstation
+		while (stocksLeft) {
+		assets.add(getNextAsset());
+		}
+		//compare values analyze them for price increase and decrease sell rules and “trimming” sell rule 
+		for (Asset a: assets) {
+			//Rule 1: Sell stock if it drops 8% its original value//
+			if (a.valueNow() < (a.getOrigValue() * (1 - getLossLimitCutoff(a)))) {
+				orderList.add(new Order(Order.Action.SELL, a.getAmount(), a.getTicker(), a.getPrice(), new Date(System.currentTimeMillis())));
+			}
+			//Rule 2: Sell stock if it grows greater than its 20% of its original value//
+			if (a.valueNow() > (a.getOrigValue() * (1 + getGrowthLimitCutoff(a)))) {
+				orderList.add(new Order(Order.Action.SELL, a.getAmount(), a.getTicker(), a.getPrice(), new Date(System.currentTimeMillis())));
+			}
+			//Rule 3: If a stock represents more than 20% of the total portfolio value, sell enough of that stock so that it represents only 15% //
+			if ((a.valueNow() / currentBalance) > getPortPortionCutoff(a)) {
+				int sellAmount = (int) ((currentBalance * getPortPortionAfterReduction(a)) / ((1 + getPortPortionAfterReduction(a)) * a.getPrice()));
+				orderList.add(new Order(Order.Action.SELL, sellAmount, a.getTicker(), a.getPrice(), new Date(System.currentTimeMillis())));
+			}
+		}
+	}
+		
+	public double getLossLimitCutoff(Asset a) {
+		return 0; //going to use sql call here//
+	}
+	
+	public double getGrowthLimitCutoff(Asset a) {
+		return 0; //going to use sql call here//
+	}
+	
+	public double getPortPortionCutoff(Asset a) {
+		return 0; //going to use sql call here//
+	}
+	
+	public double getPortPortionAfterReduction(Asset a) {
+		return 0; //going to use sql call here//
+	}
+		
+	public Asset getNextAsset() {
+		stocksLeft = false;
+		return new Asset(99.99, 200, "AAPL", BloombergAPICommunicator.Sector.INFORMATION_TECHNOLOGY);
+	}	
+	
+	public static void main(String[] args) {
+	}
+	
+}
